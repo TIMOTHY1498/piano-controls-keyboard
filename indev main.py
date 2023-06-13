8159c879a3bda6c2f011e1252408d3d6e0bfe34e
Timothy B <74752830+TIMOTHY1498@users.noreply.github.com>
2023-06-13 20:48:13 +0700

    Create indev main.py

---
+++ indev main.py
@@ -0,0 +1,31 @@
+import pynput.keyboard
+import sounddevice as sd
+import numpy as np
+
+def pitch_to_key(pitch):
+    if pitch < 440:
+        return pynput.keyboard.KeyCode.from_char('w')
+    elif pitch < 200:
+        return pynput.keyboard.KeyCode.from_char('a')
+    elif pitch < 300:
+        return pynput.keyboard.KeyCode.from_char('s')
+    elif pitch < 
+     
+def audio_callback(indata, frames, time, status):
+    pitch = calculate_pitch(indata)
+    key = pitch_to_key(pitch)
+    if key is not None:
+        controller = pynput.keyboard.Controller()
+        with controller.pressed(key):
+            pass
+          
+def calculate_pitch(audio):
+    pitch = process_audio(audio)
+    return pitch
+
+sample_rate = 44100
+duration = 0.1
+
+with sd.InputStream(callback=audio_callback, channels=1, samplerate=sample_rate):
+    sd.sleep(int(duration * 1000))
+
